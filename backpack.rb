19e29c830e56682d56a577da762ef906349a3b4d
curtischatch <curtis.chat.hk@gmail.com>
2018-08-29 13:00:03 -0400

    removed inaccurate comment

--- backpack.rb
+++ backpack.rb
@@ -11,15 +11,15 @@ class Backpack
 
   def prepare
     # set up local variables used by rest of prepare method
-    x = @attributes[:weather]
+    weather_conditon = @attributes[:weather]
     day_of_week = @attributes[:day_of_week]
 
     # Ensure appropriate clothing is added to backpack
-    if x == 'rainy'
+    if weather_condition == 'rainy'
       @items << 'pants'
       @items << 'shirt'
       @items << 'umbrella'
-    elsif x == 'cold'
+    elsif weather_conditon == 'cold'
       @items << 'pants'
       @items << 'shirt'
       @items << 'jacket'
@@ -29,7 +29,7 @@ class Backpack
     end
 
     # Ensure gym shoes are added to backpack if it's a gym day
-    # Gotta get to the gym on Tuesdays and Thursdays. Wait a sec...
+
     if day_of_week == 'monday' || day_of_week == 'thursday'
       #add gym shoes to items
       @items << 'gym shoes'
@@ -39,7 +39,6 @@ class Backpack
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     elsif false
-      # Used to bring snacks on weekend trips, but now I just buy 'em
       @items << 'snacks'
     end
   end
